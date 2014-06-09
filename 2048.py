be4a707b417d1cb674dbc1cb435d5d3ea1e03993
shrimpydubsy <new.joseph10@gmail.com>
2014-06-09 19:04:33 -0400

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,4 +1,8 @@
+from random import *
+import itertools
+
 class Game():
+    def __init__(self, height, width):
         self.height = height
         self.width = width
         self.grid = []
@@ -21,18 +25,63 @@ class Game():
             h = randint(0, len(self.grid) - 1)
             k = randint(0, len(self.grid[0]) - 1)
         self.grid[h][k] = 2
+    # adds in a new tile
+    def genTile(self):
+        y = randint(0, len(self.grid) - 1)
+        x = randint(0, len(self.grid[0]) - 1)
+        while(self.grid[y][x] > 0):
+            y = randint(0, len(self.grid) - 1)
+            x = randint(0, len(self.grid[0]) - 1)
+        self.grid[y][x] = 2
+    def isRowBlank(self, row):
+        for val in row:
+            if val != 0:
+                return False
+        return True
     def shiftUp(self):
         pass
     def shiftDown(self):
         pass
     def shiftLeft(self):
-        pass
+        # loop through each row
+        for i in range(len(self.grid)):
+            # shift the row to the left by sliding down
+            while self.grid[i][0] == 0 and not self.isRowBlank(self.grid[i]):
+                k = []
+                for z in range(len(self.grid[i])):
+                    if z > 0:
+                        k.append(self.grid[i][z])
+                k.append(0)
+                self.grid[i] = k
+        # pop in a new tile
+        self.genTile()
     def shiftRight(self):
-        pass
+        # loop through each row
+        for i in range(len(self.grid)):
+            # shift the row to the left by sliding down
+            while self.grid[i][len(self.grid[i]) - 1] == 0 and not self.isRowBlank(self.grid[i]):
+                self.grid[i].pop()
+                self.grid[i].insert(0, 0)
+        # pop in a new tile
+        self.genTile()
            
-    
+
 board = Game(4,4)
 board.set()
+
+# Print the game board
+for row in board.grid:
+    print row
+    
+board.shiftLeft()
+print "Did  a left shift"
+
+# Print the game board
+for row in board.grid:
+    print row
+
+board.shiftRight()
+print "Did  a right shift"
 
 # Print the game board
 for row in board.grid:
