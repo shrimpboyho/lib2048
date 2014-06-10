daaa18026ec9fd42c765fa639b3e60060c9891b6
shrimpboyho <shrimpboyhoftw@gmail.com>
2014-06-09 20:11:40 -0400

    Got left and right shifts semi working

--- 2048.py
+++ 2048.py
@@ -1,6 +1,47 @@
+# Used to generate random numbers
 from random import *
-import itertools
 
+# Import WConio to handle keypresses and whatnot
+import WConio
+
+# Useless nonsense
+import types
+import math
+
+# Import OS to access ability to flush stdout
+import os
+clear = lambda: os.system('cls')
+
+'''import ctypes
+
+LF_FACESIZE = 32
+STD_OUTPUT_HANDLE = -11
+
+class COORD(ctypes.Structure):
+    _fields_ = [("X", ctypes.c_short), ("Y", ctypes.c_short)]
+
+class CONSOLE_FONT_INFOEX(ctypes.Structure):
+    _fields_ = [("cbSize", ctypes.c_ulong),
+                ("nFont", ctypes.c_ulong),
+                ("dwFontSize", COORD),
+                ("FontFamily", ctypes.c_uint),
+                ("FontWeight", ctypes.c_uint),
+                ("FaceName", ctypes.c_wchar * LF_FACESIZE)]
+
+font = CONSOLE_FONT_INFOEX()
+font.cbSize = ctypes.sizeof(CONSOLE_FONT_INFOEX)
+font.nFont = 12
+font.dwFontSize.X = 11
+font.dwFontSize.Y = 18
+font.FontFamily = 54
+font.FontWeight = 400
+font.FaceName = "Lucida Console"
+
+handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+ctypes.windll.kernel32.SetCurrentConsoleFontEx(
+        handle, ctypes.c_long(False), ctypes.pointer(font)) '''
+
+# Main game class
 class Game():
     def __init__(self, height, width):
         self.height = height
@@ -46,43 +87,57 @@ class Game():
         # loop through each row
         for i in range(len(self.grid)):
             # shift the row to the left by sliding down
-            while self.grid[i][0] == 0 and not self.isRowBlank(self.grid[i]):
-                k = []
-                for z in range(len(self.grid[i])):
-                    if z > 0:
-                        k.append(self.grid[i][z])
-                k.append(0)
-                self.grid[i] = k
+            row = self.grid[i]
+            if not self.isRowBlank(row):
+                row = filter(lambda a: a != 0, row)
+                for k in range(self.width - len(row)):
+                    row.append(0)
+            self.grid[i] = row
         # pop in a new tile
         self.genTile()
     def shiftRight(self):
         # loop through each row
         for i in range(len(self.grid)):
-            # shift the row to the left by sliding down
-            while self.grid[i][len(self.grid[i]) - 1] == 0 and not self.isRowBlank(self.grid[i]):
-                self.grid[i].pop()
-                self.grid[i].insert(0, 0)
+            # shift the row to the right by sliding down
+            row = self.grid[i]
+            if not self.isRowBlank(row):
+                row = filter(lambda a: a != 0, row)
+                for k in range(self.width - len(row)):
+                    row.insert(0,0)
+            self.grid[i] = row
         # pop in a new tile
         self.genTile()
            
 
+# Create Game
 board = Game(4,4)
 board.set()
 
-# Print the game board
-for row in board.grid:
-    print row
-    
-board.shiftLeft()
-print "Did  a left shift"
+# Prints the graphics to the console
+def printGraphics(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print '\n'.join(table)
 
-# Print the game board
-for row in board.grid:
-    print row
+# Enter input loop
+key = ' '
+while key != 'q':
+    # Print the game board
+    clear()
+    print "2048.py (Use arrow keys. Enter 'q' to quit.)"
+    #printGraphics(board.grid)
+    for row in board.grid:
+        print row
 
-board.shiftRight()
-print "Did  a right shift"
-
-# Print the game board
-for row in board.grid:
-    print row
+    # Get the key press
+    key = WConio.getkey()
+    if key == 'up':
+        pass
+    elif key == 'down':
+        pass
+    elif key == 'left':
+        board.shiftLeft()
+    elif key == 'right':
+        board.shiftRight()
